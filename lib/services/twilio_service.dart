1d18814c2c1a6cf63cda12bb06d1818dd376fa84
nich.otieno <nich.otieno@gmail.com>
2025-04-14 15:33:26 +0300

    implement CallKit integration for incoming calls and enhance event handling

--- lib/services/twilio_service.dart
+++ lib/services/twilio_service.dart
@@ -3,6 +3,11 @@ import 'dart:developer';
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_callkit_incoming/entities/android_params.dart';
+import 'package:flutter_callkit_incoming/entities/call_kit_params.dart';
+import 'package:flutter_callkit_incoming/entities/ios_params.dart';
+import 'package:flutter_callkit_incoming/entities/notification_params.dart';
+import 'package:flutter_callkit_incoming/flutter_callkit_incoming.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:logger/logger.dart';
 import 'package:twilio_voice/twilio_voice.dart';
@@ -138,7 +143,7 @@ class TwilioService {
   /// Setup listeners for Twilio events
   void _setupListeners(BuildContext context) {
     // Listen for Twilio Call Events
-    TwilioVoice.instance.callEventsListener.listen((event) {
+    TwilioVoice.instance.callEventsListener.listen((event) async {
       switch (event) {
         case CallEvent.incoming:
           log("Incoming Call detected!");
@@ -149,7 +154,66 @@ class TwilioService {
         case CallEvent.connected:
           log("Call Connected!");
           Logger().i("Twillio Call Event: $event");
-          _stopRingtone(); // Stop ringtone when call is connected
+          _stopRingtone();
+          CallKitParams callKitParams = CallKitParams(
+            id: "",
+            nameCaller: 'Hien Nguyen',
+            appName: 'Callkit',
+            avatar: 'https://i.pravatar.cc/100',
+            handle: '0123456789',
+            type: 0,
+            textAccept: 'Accept',
+            textDecline: 'Decline',
+            missedCallNotification: NotificationParams(
+              showNotification: true,
+              isShowCallback: true,
+              subtitle: 'Missed call',
+              callbackText: 'Call back',
+            ),
+            callingNotification: const NotificationParams(
+              showNotification: true,
+              isShowCallback: true,
+              subtitle: 'Calling...',
+              callbackText: 'Hang Up',
+            ),
+            duration: 30000,
+            extra: <String, dynamic>{'userId': '1a2b3c4d'},
+            headers: <String, dynamic>{
+              'apiKey': 'Abc@123!',
+              'platform': 'flutter'
+            },
+            android: const AndroidParams(
+                isCustomNotification: true,
+                isShowLogo: false,
+                logoUrl: 'https://i.pravatar.cc/100',
+                ringtonePath: 'system_ringtone_default',
+                backgroundColor: '#0955fa',
+                backgroundUrl: 'https://i.pravatar.cc/500',
+                actionColor: '#4CAF50',
+                textColor: '#ffffff',
+                incomingCallNotificationChannelName: "Incoming Call",
+                missedCallNotificationChannelName: "Missed Call",
+                isShowCallID: false),
+            ios: IOSParams(
+              iconName: 'CallKitLogo',
+              handleType: 'generic',
+              supportsVideo: true,
+              maximumCallGroups: 2,
+              maximumCallsPerCallGroup: 1,
+              audioSessionMode: 'default',
+              audioSessionActive: true,
+              audioSessionPreferredSampleRate: 44100.0,
+              audioSessionPreferredIOBufferDuration: 0.005,
+              supportsDTMF: true,
+              supportsHolding: true,
+              supportsGrouping: false,
+              supportsUngrouping: false,
+              ringtonePath: 'system_ringtone_default',
+            ),
+          );
+          await FlutterCallkitIncoming.showCallkitIncoming(
+              callKitParams); // Stop ringtone when call is connected
+         
           break;
         case CallEvent.callEnded:
           log("Call Ended!");
