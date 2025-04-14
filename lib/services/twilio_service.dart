e577e295b8bb5d97dfedcc23c690aba693b78ca2
nich.otieno <nich.otieno@gmail.com>
2025-04-14 19:31:03 +0300

    refactor: remove redundant CallKitParams initialization in incoming call handling

--- lib/services/twilio_service.dart
+++ lib/services/twilio_service.dart
@@ -151,62 +151,7 @@ class TwilioService {
           log("Incoming Call detected!");
           Logger().i("Twillio Call Event: $event");
           _stopRingtone();
-          CallKitParams callKitParams = CallKitParams(
-            id: _currentUUID,
-            appName: 'Callkit',
-            avatar: 'https://i.pravatar.cc/100',
-            handle: '0123456789',
-            type: 0,
-            textAccept: 'Accept',
-            textDecline: 'Decline',
-            missedCallNotification: NotificationParams(
-              showNotification: true,
-              isShowCallback: true,
-              subtitle: 'Missed call',
-              callbackText: 'Call back',
-            ),
-            callingNotification: const NotificationParams(
-              showNotification: true,
-              isShowCallback: true,
-              subtitle: 'Calling...',
-              callbackText: 'Hang Up',
-            ),
-            duration: 30000,
-            extra: <String, dynamic>{'userId': '1a2b3c4d'},
-            headers: <String, dynamic>{
-              'apiKey': 'Abc@123!',
-              'platform': 'flutter'
-            },
-            android: const AndroidParams(
-                isCustomNotification: true,
-                isShowLogo: false,
-                logoUrl: 'https://i.pravatar.cc/100',
-                ringtonePath: 'system_ringtone_default',
-                backgroundColor: '#0955fa',
-                backgroundUrl: 'https://i.pravatar.cc/500',
-                actionColor: '#4CAF50',
-                textColor: '#ffffff',
-                incomingCallNotificationChannelName: "Incoming Call",
-                missedCallNotificationChannelName: "Missed Call",
-                isShowCallID: false),
-            ios: IOSParams(
-              iconName: 'CallKitLogo',
-              handleType: 'generic',
-              supportsVideo: true,
-              maximumCallGroups: 2,
-              maximumCallsPerCallGroup: 1,
-              audioSessionMode: 'default',
-              audioSessionActive: true,
-              audioSessionPreferredSampleRate: 44100.0,
-              audioSessionPreferredIOBufferDuration: 0.005,
-              supportsDTMF: true,
-              supportsHolding: true,
-              supportsGrouping: false,
-              supportsUngrouping: false,
-              ringtonePath: 'system_ringtone_default',
-            ),
-          );
-          await FlutterCallkitIncoming.showCallkitIncoming(callKitParams);
+
           break;
         case CallEvent.connected:
           log("Call Connected!");
