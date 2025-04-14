723f4239e2c647491c236023b2d43e33a053aa81
nich.otieno <nich.otieno@gmail.com>
2025-04-14 18:29:46 +0300

    refactor AppDelegate to enhance VoIP push handling and improve incoming call data processing

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,9 @@ import UIKit
 import Firebase
 import FirebaseCore
 import FirebaseMessaging
+import PushKit
+import flutter_callkit_incoming
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -13,41 +16,50 @@ import FirebaseMessaging
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     // Register for VoIP push
-        let voipRegistry = PKPushRegistry(queue: DispatchQueue.main)
+        let mainQueue = DispatchQueue.main
+        let voipRegistry: PKPushRegistry = PKPushRegistry(queue: mainQueue)
         voipRegistry.delegate = self
-        voipRegistry.desiredPushTypes = [.voIP]
+        voipRegistry.desiredPushTypes = [PKPushType..voIP]
 
         
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
-        TwilioVoiceSDK.registerAccessToken(
-            "YOUR_TWILIO_TOKEN", // Replace this with token from your server
-            deviceToken: pushCredentials.token,
-            completion: { error in
-                print("VoIP token registered: \(error?.localizedDescription ?? "Success")")
-            }
-        )
+  // Handle updated push credentials
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+        print(credentials.token)
+        let deviceToken = credentials.token.map { String(format: "%02x", $0) }.joined()
+        print(deviceToken)
+        //Save deviceToken to your server
+        SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP(deviceToken)
     }
 
-    func pushRegistry(_ registry: PKPushRegistry,
-                      didReceiveIncomingPushWith payload: PKPushPayload,
-                      for type: PKPushType,
-                      completion: @escaping () -> Void) {
+    func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
+        print("didInvalidatePushTokenFor")
+        SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP("")
+    }
 
-        let uuid = UUID().uuidString
-        let callerName = "Twilio User"
-
-        if let controller = window?.rootViewController as? FlutterViewController {
-            let channel = FlutterMethodChannel(name: "com.example.callkit", binaryMessenger: controller.binaryMessenger)
-            channel.invokeMethod("incoming", arguments: [
-                "uuid": uuid,
-                "callerName": callerName
-            ])
+     // Handle incoming pushes
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
+        print("didReceiveIncomingPushWith")
+        guard type == .voIP else { return }
+        
+        let id = payload.dictionaryPayload["id"] as? String ?? ""
+        let nameCaller = payload.dictionaryPayload["nameCaller"] as? String ?? ""
+        let handle = payload.dictionaryPayload["handle"] as? String ?? ""
+        let isVideo = payload.dictionaryPayload["isVideo"] as? Bool ?? false
+        
+        let data = flutter_callkit_incoming.Data(id: id, nameCaller: nameCaller, handle: handle, type: isVideo ? 1 : 0)
+        //set more data
+        data.extra = ["user": "abc@123", "platform": "ios"]
+        //data.iconName = ...
+        //data.....
+        SwiftFlutterCallkitIncomingPlugin.sharedInstance?.showCallkitIncoming(data, fromPushKit: true)
+        
+        //Make sure call completion()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            completion()
         }
-
-        completion()
     }
 
 }
