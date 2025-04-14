24c3c21e2b5421996ff86bb3b792d0696c436aaa
nich.otieno <nich.otieno@gmail.com>
2025-04-15 00:37:03 +0300

    refactor: update logging for incoming push payload in pushRegistry handler

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -44,7 +44,7 @@ import flutter_callkit_incoming
 
      // Handle incoming pushes
     @objc(pushRegistry:didReceiveIncomingPushWithPayload:forType:withCompletionHandler:) func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
-        print("didReceiveIncomingPushWith")
+        print("payload \(payload.dictionaryPayload)")
         guard type == .voIP else { return }
         
         let id = payload.dictionaryPayload["id"] as? String ?? ""
